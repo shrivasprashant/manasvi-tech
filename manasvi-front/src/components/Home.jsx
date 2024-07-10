3f96eafe279cfaa8f6c2264e082b0950989a24db
basa738997 <100225179+basa738997@users.noreply.github.com>
2024-07-10 14:32:17 +0530

    sevices added in home

--- manasvi-front/src/components/Home.jsx
+++ manasvi-front/src/components/Home.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Footer from './Footer'
 import ContactForm from './ContactForm'
 import ProductCarousel from './ProductCarousel'
+import ServicesPage from './ServicesPage'
 
 function Home() {
   return (
     <div>
+      <ServicesPage />
       <ProductCarousel />
       <ContactForm />
       <Footer />
